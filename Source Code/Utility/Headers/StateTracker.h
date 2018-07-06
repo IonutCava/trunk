d2eb6073670afe27edf0bd955c8bed6a1ac0c493
IonutCava <IonutCava@e89c6373-eeba-4702-abef-d26284fe765c>
2018-07-06 05:04:03 +0100

    [Ionut][[BR]] - Added missing StateTracker.h [[BR]]
    git-svn-id: https://xp-dev.com/svn/Divide-Framework/trunk@282 e89c6373-eeba-4702-abef-d26284fe765c
    
    Former-commit-id: dac45a9f93b6570d36ce860a491e279696c2edb7 [formerly f18bc0bb597fe0bf3222303e094e9d9952d4da2a] [formerly 97564d0bb29f2b97c083a716da0c8c180a00e992 [formerly 02de4a26649f757c3e0400d239f0b33a62ec7720]]
    Former-commit-id: 5a92a74a3296819ffdab5a941ee31107bd3efd20 [formerly 61f3ec2d85f4316d99560e871fa24edb84ef5d2d]
    Former-commit-id: d9396a53c14559c989289f73c0172617b2b81da0

---
+++ Source Code/Utility/Headers/StateTracker.h
@@ -0,0 +1,74 @@
+/*
+   Copyright (c) 2014 DIVIDE-Studio
+   Copyright (c) 2009 Ionut Cava
+
+   This file is part of DIVIDE Framework.
+
+   Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+   and associated documentation files (the "Software"), to deal in the Software without restriction,
+   including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+   and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+   subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+   INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+   IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+   WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+   OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ */
+
+#ifndef UTIL_STATE_TRACKER_H_
+#define UTIL_STATE_TRACKER_H_
+
+#include "Vector.h"
+
+template<class T>
+class StateTracker {
+public:
+    StateTracker() 
+    {
+        _trackedValues.reserve(16);
+    }
+
+    ~StateTracker()
+    {
+        _trackedValues.clear();
+    }
+
+    inline T getTrackedValue(U32 index) { 
+        while(index >= _trackedValues.size()){
+            _trackedValues.push_back(optionalValue());
+        }
+        return _trackedValues[index].value;  
+    }
+
+    inline void setTrackedValue(U32 index, T value)       { 
+        _trackedValues[index].value = value; 
+        _trackedValues[index].initialized = true;
+    }
+
+    /// Init will not change an already initialized value
+    inline void initTrackedValue(U32 index, const T value) {
+        getTrackedValue(index);
+        if(!_trackedValues[index].initialized)
+            setTrackedValue(index, value);
+    }
+
+protected:
+    struct optionalValue{
+        T value;
+        bool initialized;
+        optionalValue() : initialized(false) 
+        {
+        }
+    };
+
+protected:
+    vectorImpl<optionalValue > _trackedValues;
+
+};
+
+#endif
