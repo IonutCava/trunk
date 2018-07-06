fe22ce5fd5ae5456898ee6610197bdde5f22ece5
IonutCava <IonutCava@e89c6373-eeba-4702-abef-d26284fe765c>
2018-07-06 05:04:03 +0100

    [Ionut] - Engine libraries now need to sit in a "3rdParty" folder inside our working directory
    git-svn-id: https://xp-dev.com/svn/Divide-Framework/trunk@954 e89c6373-eeba-4702-abef-d26284fe765c
    
    Former-commit-id: 3ce663be07efa33c06ee2021d06f7dbc9470d598 [formerly c90a275c1f65830a4310bfcf46991a167a8d8074] [formerly 1a2c7ea6baedf9a4f1f88cd8b11fd5d19aca3a51 [formerly 40ebe586bb7f27347a48ee7b90f4180f94ac40f1]]
    Former-commit-id: 57dfd23085ed4f00646d2d3db948b41e8171cdca [formerly f59acdfc762bb080ca693757c567d67cc67832d1]
    Former-commit-id: 62227d4ad789e5238b6b73f187bb8b5ba70d52b4

--- InstallLibraries.bat
+++ InstallLibraries.bat
@@ -1,16 +1,12 @@
 @echo off
-echo Downloading third party libraries
 
-if "%~1"=="" (
-	set libraryLocation=%~dp0..\Libraries
-) else (
-	set libraryLocation=%1
-)
+set libraryFolder=3rdParty
+set libraryLocation=%~dp0%libraryFolder%
 
 IF NOT EXIST %libraryLocation% mkdir %libraryLocation%
-cd %libraryLocation%
 
-@echo off
+echo Downloading third party libraries into folder: %libraryLocation%
+
 svn >nul 2>nul
 if %ERRORLEVEL% neq 9009 goto continue
 echo Subversion "svn" command not found. Please install Subversion first..
@@ -19,14 +15,13 @@ exit 1
 
 :continue
 svn cleanup
-svn checkout https://xp-dev.com/svn/Divide-Dependencies %libraryLocation%
-echo Setting $(EngineLibraries) system variable to %libraryLocation%
-setx EngineLibraries %libraryLocation%
+svn checkout https://xp-dev.com/svn/Divide-Dependencies %libraryFolder%
+
 IF NOT EXIST physx mkdir physx
 echo Downloading PhysX SDK (manually register and download the newest version)
 echo Build all of the projects in DEBUG, CHECKED and RELEASE mode
 echo Remember to update the PhysX DLL in the working directory with the newly built variants
 start explorer "https://developer.nvidia.com/physx-source-github"
-echo Copy the contents of the SDK to "$(EngineLibraries)/physx"
+echo Copy the contents of the SDK to "3rdParty/physx"
 echo Make sure you have the OpenAL runtime installed as well: https://www.openal.org/downloads/
 pause
