4547a45f21551d5a16c757b9461b174626c9cdaf
IonutCava <IonutCava@e89c6373-eeba-4702-abef-d26284fe765c>
2018-07-06 05:04:03 +0100

    [IonutCava] - Fixed a typo in RingBuffer class - Update PhysX dlls - Improve InstallLibraries.bat batch file
    git-svn-id: https://xp-dev.com/svn/Divide-Framework/trunk@751 e89c6373-eeba-4702-abef-d26284fe765c
    
    Former-commit-id: f6b18b40760eb38c71a962e87e347208b50c076d [formerly 4a1b6eaafa7262df126273a6e2ef02fed529148e] [formerly d22d25026c571fd524582a86ddcf7a35897eb64a [formerly 3220d6ef1f384e9e8a3d3f96da2de87e67f6d68c]]
    Former-commit-id: e7d5f2956a6f6b1000949520205b76d18a7ef42d [formerly 7976e1f69fdacaf0dec19f40140ad7b0174027c0]
    Former-commit-id: c74e0bc8d847da3b5a5f9321b7b6b839fb09c53d

--- InstallLibraries.bat
+++ InstallLibraries.bat
@@ -7,13 +7,25 @@ if "%~1"=="" (
 	set libraryLocation=%1
 )
 
-mkdir %libraryLocation%
+IF NOT EXIST %libraryLocation% mkdir %libraryLocation%
 cd %libraryLocation%
+
+@echo off
+svn >nul 2>nul
+if %ERRORLEVEL% neq 9009 goto continue
+echo Subversion "svn" command not found. Please install Subversion first..
+pause
+exit 1
+
+:continue
 svn cleanup
 svn checkout https://xp-dev.com/svn/Divide-Dependencies %libraryLocation%
 echo Setting $(EngineLibraries) system variable to %libraryLocation%
 setx EngineLibraries %libraryLocation%
 echo Downloading PhysX SDK (manually register and download the newest version)
+echo Build all of the projects in DEBUG, CHECKED and RELEASE mode
+echo Remember to update the PhysX DLL in the working directory with the newly built variants
 start explorer "https://developer.nvidia.com/physx-sdk"
 echo Copy the contents of the SDK to "$(EngineLibraries)/physx"
+echo Make sure you have the OpenAL runtime installed as well: https://www.openal.org/downloads/
 pause
