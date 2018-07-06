ff0abbd74e4a5f06393d3a30170784e130743b9e
k1ngp1n <k1ngp1n@e89c6373-eeba-4702-abef-d26284fe765c>
2018-07-06 05:04:03 +0100

    [k1ngp1n] -Forgot to add the Water source files
    git-svn-id: https://xp-dev.com/svn/Divide-Framework/trunk@68 e89c6373-eeba-4702-abef-d26284fe765c
    
    Former-commit-id: 091826ab8a2045c76c7f2eecd1aed77d0542a44c [formerly a4cfc94322cbf6f555e05043733cb045c6e80bf7]
    Former-commit-id: 002a62ae2a5126046900e8f6c8dd7e2cec77bd41

---
+++ Cod Sursa/Terrain/Water.cpp
@@ -0,0 +1,55 @@
+#include "Water.h"
+#include "Managers/ResourceManager.h"
+#include "TextureManager/Texture2D.h"
+#include "Rendering/common.h"
+#include "Hardware/Video/GFXDevice.h"
+
+WaterPlane::WaterPlane() : _texture(ResourceManager::getInstance().LoadResource<Texture2D>(ParamHandler::getInstance().getParam<string>("assetsLocation")+"/misc_images/terrain_water_NM.jpg")),
+						   _shader(ResourceManager::getInstance().LoadResource<Shader>("water"))
+
+{
+	_shininess =  50.0f;
+	_noiseTile = 10.0f;
+	_noiseFactor = 0.1f;
+	_plane = new Quad3D(vec3(-1,0,1),vec3(1,0,1),vec3(-1,0,-1),vec3(1,0,-1));
+}
+
+bool WaterPlane::unload()
+{
+	bool state = false;
+	delete _plane;
+	state = _shader->unload();
+	state = _texture->unload();
+	return state;
+}
+
+void WaterPlane::draw(FrameBufferObject& _fbo)
+{
+	//ToDo:  Remove OpenGL commands:
+
+	glPushAttrib(GL_ENABLE_BIT);
+	glDisable(GL_CULL_FACE);
+	glEnable(GL_BLEND);
+
+
+	_fbo.Bind(0);
+	_texture->Bind(1);
+	_shader->bind();
+		_shader->UniformTexture("texWaterReflection", 0);
+		_shader->UniformTexture("texWaterNoiseNM", 1);
+		_shader->Uniform("time", GETTIME());
+		_shader->Uniform("water_shininess",_shininess );
+		_shader->Uniform("noise_tile", _noiseTile );
+		_shader->Uniform("noise_factor", _noiseFactor);
+		_shader->Uniform("win_width",  Engine::getInstance().getWindowWidth());
+		_shader->Uniform("win_height", Engine::getInstance().getWindowHeight());
+		_shader->Uniform("bbox_min",_plane->_tl);
+		_shader->Uniform("bbox_max",_plane->_br);
+
+	GFXDevice::getInstance().drawQuad3D(_plane);
+	_shader->unbind();
+	_texture->Unbind(1);
+	_fbo.Unbind(0);
+
+	glPopAttrib();
+}
